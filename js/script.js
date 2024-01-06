aef7d29bbfe399155f8358c7efe65dbc8bb8ea1a
zain sheikh <sanjhana@ideabox.pk>
2024-01-06 14:48:44 +0500

    chnages

--- js/script.js
+++ js/script.js
@@ -143,3 +143,28 @@ var swiper3 = new Swiper(".team-slider", {
     prevEl: ".team-slider-prev",
   },
 });
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function (e) {
+  e.preventDefault();
+  modal.style.display = "flex";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
